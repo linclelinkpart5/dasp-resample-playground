b7029a256295fbe82a1d103670b179991c9759a0
Mark LeMoine <linclelinkpart5@gmail.com>
2021-04-16 18:19:00 -0700

    Section out `dasp` impl to allow easier add of `sampara` impl

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,14 @@ use std::fs::OpenOptions;
 use std::io::Cursor;
 
 use byteorder::{NativeEndian, ReadBytesExt, WriteBytesExt};
-use dasp_frame::Frame;
-use dasp_sample::Sample;
-use dasp_signal::Signal;
-use dasp_ring_buffer::Fixed;
-use dasp_interpolate::sinc::Sinc;
 
 const NUM_CHANNELS: usize = 2;
 const SOURCE_PATH: &str = "/home/mark/source.raw";
 const TARGET_PATH: &str = "/home/mark/target.raw";
 
-fn main() {
+const SINC_BUFFER: [[f32; NUM_CHANNELS]; 128] = [[0f32; NUM_CHANNELS]; 128];
+
+fn read_int_frames() -> impl Iterator<Item = [i32; NUM_CHANNELS]> {
     let bytes = std::fs::read(SOURCE_PATH).unwrap();
 
     println!("Read {} bytes", bytes.len());
@@ -25,16 +22,13 @@ fn main() {
         // We assume that the only error that occurs is when trying to read
         // from the cursor when it is already empty.
         reader.read_i32_into::<NativeEndian>(&mut buf).ok()?;
-        Some(Frame::map(buf, f32::from_sample))
+        Some(buf)
     });
 
-    let signal = dasp_signal::from_iter(frames);
+    frames
+}
 
-    let ring_buffer = Fixed::from([[0f32; NUM_CHANNELS]; 128]);
-    let interpolator = Sinc::new(ring_buffer);
-
-    let interpolated_signal = signal.from_hz_to_hz(interpolator, 44100.0, 48000.0);
-
+fn write_int_frames(frames: impl Iterator<Item = [i32; NUM_CHANNELS]>) {
     let mut out_file = OpenOptions::new()
         .create(true)
         .write(true)
@@ -42,26 +36,53 @@ fn main() {
         .open(TARGET_PATH)
         .unwrap();
 
-    let mut num_clipped = 0;
-    for frame in interpolated_signal.until_exhausted() {
+    for frame in frames {
+        for sample in std::array::IntoIter::new(frame) {
+            out_file.write_i32::<NativeEndian>(sample).unwrap();
+        }
+    }
+}
+
+fn dasp_impl(int_frames: impl Iterator<Item = [i32; NUM_CHANNELS]>) -> impl Iterator<Item = [i32; NUM_CHANNELS]> {
+    use dasp_frame::Frame;
+    use dasp_sample::Sample;
+    use dasp_signal::Signal;
+    use dasp_ring_buffer::Fixed;
+    use dasp_interpolate::sinc::Sinc;
+
+    let frames = int_frames.map(|frame| Frame::map(frame, f32::from_sample));
+    let signal = dasp_signal::from_iter(frames);
+
+    let ring_buffer = Fixed::from(SINC_BUFFER);
+    let interpolator = Sinc::new(ring_buffer);
+
+    let interpolated_signal = signal.from_hz_to_hz(interpolator, 44100.0, 48000.0);
+
+    let clipped_signal = interpolated_signal.map(|frame| {
         let clipped_frame: [f32; NUM_CHANNELS] = Frame::map(frame, |sample| {
             if sample > 1.0 {
-                num_clipped += 1;
+                println!("Clipping detected: {}", sample);
                 1.0
             } else if sample < -1.0 {
-                num_clipped += 1;
+                println!("Clipping detected: {}", sample);
                 -1.0
             } else {
                 sample
             }
         });
 
-        for sample in clipped_frame.channels() {
-            let int_sample: i32 = sample.to_sample();
+        clipped_frame
+    });
 
-            out_file.write_i32::<NativeEndian>(int_sample).unwrap();
-        }
-    }
+    let output_signal = clipped_signal.map(|frame| Frame::map(frame, i32::from_sample));
 
-    println!("Clipped samples: {}", num_clipped);
+    output_signal.until_exhausted()
+}
+
+fn main() {
+    let the_impl = dasp_impl;
+
+    let in_int_frames = read_int_frames();
+    let out_int_frames = the_impl(in_int_frames);
+    write_int_frames(out_int_frames);
 }
